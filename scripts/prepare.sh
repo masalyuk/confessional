e6ead8f4f0672fd11c33071a85e4f28c8e15f10f
Nikita Masaliuk <nikita.masalyuk@mail.ru>
2024-02-01 23:58:32 +0200

    Install pip in requirements

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -4,6 +4,7 @@ sudo apt-get upgrade
 
 sudo apt-get install git
 sudo apt-get install python
+sudo apt-get install python3-pip
 
 sudo apt-get install baresip
 sudo apt-get install ffmpeg
@@ -13,8 +14,3 @@ sudo apt-get install libasound2-dev
 
 # need to activate python env
 pip3 install -r requirements.txt
-
-
-
-
-
